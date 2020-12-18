6d1a19364bc493336cf8fffb713d6abd0f58b00d
Giovanni Garcia <giovannigarciar@hotmail.com>
2020-12-17 21:19:34 -0800

    Added skeleton functions

--- src/main.c
+++ src/main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
+#include "global.h"
 
 int main(int argc, char const *argv[])
 {
     printf("Hello World\n");
     
     return 0;
+}
+
+uint8_t getFireData(void)
+{
+    
+    return ERROR;
+}
+
+uint8_t getCovidData(void)
+{
+
+    return ERROR;
+}
+
+uint8_t getAirQltyData(void)
+{
+
+    return ERROR;
+}
+
+uint8_t setupSMS()
+{
+
+    return ERROR;
 }

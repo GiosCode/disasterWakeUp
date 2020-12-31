11b13bb0c63b1feada90cae7cbb3a3490a0037a8
Giovanni Garcia <giovannigarciar@hotmail.com>
2020-12-30 20:41:27 -0800

    Able to select time for notification. Can create payload. Need better data.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include "global.h"
 #include <curl/curl.h>
-#include <json-c/json.h>
+#include <json-c/json.h>//maybe delete
 #include <string.h>
+#include <time.h>
 
 uint8_t getFireData(void);
 uint8_t sendText(char const *email, char const *password, char const *phoneEmail, char const *mailServer, FILE *payload);
@@ -13,12 +14,37 @@ const char *passwd;
 const char *phEmail;
 const char *mailServ;
 const char *zipCode;
+struct tm  *timeinfo;
+
+struct userInfo
+{
+    const char *myEmail;
+    const char *passwd;
+    const char *phEmail;
+    const char *mailServ;
+    const char *zipCode;   
+};
 
 int main(int argc, char const *argv[])
 {
     //TODO: Extend to any location
     //TODO: Add variable notifiaiton time (V2)
     
+    //Start time
+    time_t timeRaw;
+    
+    timeRaw = time(NULL);
+    timeinfo = localtime(&timeRaw);
+    while (1)
+    {
+        timeRaw = time(NULL);
+        timeinfo = localtime(&timeRaw);
+        if ((timeinfo->tm_hour == 15)&&(timeinfo->tm_min == 40))
+        {
+            printf("The local time is %s\n", asctime(timeinfo));
+            break;
+        }
+    }
     //Format "zipCode senderEmail "senderEmailPassword" PhoneEmail MailServer"
     zipCode  = argv[1];
     myEmail  = argv[2];
@@ -29,14 +55,14 @@ int main(int argc, char const *argv[])
     //TODO: Need to verify they pass in a valid values
     FILE *payload;
     printf("Getting information for:\nZipCode: %s\nEmail: %s\nPassword: %s\nPhoneEmail: %s\nmailServer: %s\n", zipCode, myEmail, passwd, phEmail, mailServ);
-    makePayload(payload);
-    sendText(myEmail,passwd,phEmail,mailServ, payload);
     if(getFireData() == ERROR)
     {
         printf("Error getting fire data\n");
         return 0;
     }
-    
+    makePayload(payload);
+    sendText(myEmail,passwd,phEmail,mailServ, payload);
+
     return 0;
 }
 /*
@@ -102,7 +128,7 @@ uint8_t sendText(char const *email, char const *password, char const *phoneEmail
         if (payload == NULL)
         {
             curl_easy_cleanup(curl);
-            printf("Error opening file payload");
+            printf("Error opening payload\n");
             fclose(payload);
             return ERROR;
         }
@@ -132,23 +158,22 @@ uint8_t sendText(char const *email, char const *password, char const *phoneEmail
         {
             fprintf(stderr, "curl_easy_perform() failed: %s\n",
             curl_easy_strerror(res));
-
             /* Free recipient list */
-             curl_slist_free_all(recipients);
-
+            curl_slist_free_all(recipients);
             /* Cleanup curl */
             curl_easy_cleanup(curl);
             /* Close File */
             fclose(payload);
             return ERROR;
         }
-        
+
+        //Close file
         fclose(payload);
         /* Free recipient list */
         curl_slist_free_all(recipients);
-
         /* Cleanup curl */
         curl_easy_cleanup(curl);
+
         return OK;
     }
 
@@ -158,7 +183,7 @@ uint8_t sendText(char const *email, char const *password, char const *phoneEmail
 uint8_t makePayload(FILE *payload)
 {
     payload = fopen("payload.txt", "w+");
-    fprintf(payload, "From: John Smith <john@example.com>\nTo: Joe Smith <smith@example.com>\nSubject: No Space\n Hey there");
+    fprintf(payload, "From: DisasterWakeup <%s>\nTo: Me <%s>\nSubject: No Space\n Hey there the time is %s", myEmail, phEmail, asctime(timeinfo));
     fprintf(payload,"\n\nSomething something something");
     fclose(payload);
     return OK;
